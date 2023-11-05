213a5addbbda5fd8db2232f23b3dbdcbfa0546a3
Mike Caron <mike.caron@patientpop.com>
2023-11-05 01:16:27 -0500

    fix #toc to work in html blocks (i.e, includes)

--- src/toc.mjs
+++ src/toc.mjs
@@ -1,5 +1,7 @@
 import { repeatString } from "./util.mjs";
 
+const tocMarker = /#toc(?:\s+(\d))?/;
+
 export function handleToc(tokens, md) {
     const headings = [];
     let minLevel = 7;
@@ -16,44 +18,75 @@ export function handleToc(tokens, md) {
 
     // now, look for #toc
     for(const token of tokens) {
-        if(token.type === "inline" && token.content.indexOf("#toc") === 0) {
-            const levelStr = token.content.substring(4);
-            let level = minLevel;
-            if(levelStr) {
-                level = parseInt(levelStr, 10);
-            }
-            
-            let newText = "";
-            for(const heading of headings) {
-                const next = tokens[tokens.indexOf(heading) + 1];
-                let id = null;
-                for(const [name, value] of heading.attrs) {
-                    if(name === "id") {
-                        id = value;
-                        break;
-                    }
-                }
-                const absLevel = parseInt(heading.tag.substring(1), 10);
-                if(absLevel > level) {
-                    continue;
-                }
-                const thisLevel = parseInt(heading.tag.substring(1), 10) - minLevel;
-                const indent = repeatString("   ", thisLevel);
-                newText += `${indent}1. [${next.content}](#${id})\n`;
-            }
-            // console.log(newText);
-            const newTokens = md.parse(newText);
-
-            // console.log(newTokens[0]);
-
-            if(!newTokens[0].attrs) {
-                newTokens[0].attrs = [];
+        if(token.type === "inline" || token.type === "html_block") {
+            const result = tocMarker.exec(token.content);
+            if(!result) {
+                continue;
             }
 
-            newTokens[0].attrs.push(["class", "table-of-contents"]);
+            const newTokens = buildTocTokens(headings, tokens, minLevel, result[1], md);
+
+            const parts = token.content.split(tocMarker);
+
+            console.log(parts);
+
+            if(parts[0]) {
+                newTokens.unshift({
+                    ...token,
+                    content: parts[0],
+                })
+            };
+
+            if(parts[parts.length - 1]) {
+                newTokens.push({
+                    ...token,
+                    content: parts[parts.length - 1],
+                })
+            }
 
             tokens.splice(tokens.indexOf(token), 1, ...newTokens);
         }
     }
 
+}
+
+function buildTocTokens(headings, tokens, minLevel, levelStr, md) {
+    let level = minLevel;
+    if(levelStr) {
+        level = parseInt(levelStr, 10);
+    }
+
+    const newText = buildTocText(headings, tokens, minLevel, level);
+    const newTokens = md.parse(newText);
+
+    if(!newTokens[0].attrs) {
+        newTokens[0].attrs = [];
+    }
+
+    newTokens[0].attrs.push(["class", "table-of-contents"]);
+
+    return newTokens;
+}
+
+function buildTocText(headings, tokens, minLevel, level) {
+    let newText = "";
+    for(const heading of headings) {
+        const next = tokens[tokens.indexOf(heading) + 1];
+        let id = null;
+        for(const [name, value] of heading.attrs) {
+            if(name === "id") {
+                id = value;
+                break;
+            }
+        }
+        const absLevel = parseInt(heading.tag.substring(1), 10);
+        if(absLevel > level) {
+            continue;
+        }
+        const thisLevel = parseInt(heading.tag.substring(1), 10) - minLevel;
+        const indent = repeatString("   ", thisLevel);
+        newText += `${indent}1. [${next.content}](#${id})\n`;
+    }
+
+    return newText;
 }
